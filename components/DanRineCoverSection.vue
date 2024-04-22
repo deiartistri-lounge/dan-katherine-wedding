c05ca375fdef9d36adc581c4c0bf0cf00bb5c728
Edz Lazaro <dev.edzlazaro@gmail.com>
2024-04-22 18:17:56 +0800

    Test Fix

--- components/DanRineCoverSection.vue
+++ components/DanRineCoverSection.vue
@@ -40,105 +40,22 @@ sound.on("end", function () {
 });
 </script>
 <template>
-  <div class="w-[100vw] h-[100vh] top-0 bottom-0 right-0 left-0">
-    <section
-      id="danrine-cover"
-      ref="target"
-      class="relative flex min-h-screen p-3 bg-center bg-cover lg:bg-fixed bg-danrine1"
+  <section
+    id="danrine-cover"
+    ref="target"
+    class="relative flex min-h-screen p-3"
+  >
+    <div
+      class="absolute top-0 bottom-0 left-0 right-0 bg-fixed bg-center bg-cover bg-danrine1"
+    ></div>
+    <div
+      class="flex flex-col items-center justify-center w-full min-h-screen text-center text-primary-50"
     >
       <div
-        class="flex flex-col items-center justify-center w-full min-h-screen text-center text-primary-50"
+        class="flex flex-col items-center justify-center gap-6 mt-6 lg:p-6 lg:mt-0"
       >
         <div
-          class="flex flex-col items-center justify-center gap-6 mt-6 lg:p-6 lg:mt-0"
-        >
-          <div
-            class="text-2xl text-white lg:text-6xl drop-shadow font-cursive"
-            v-motion
-            :initial="{
-              opacity: 0,
-              scale: 0,
-            }"
-            :visibleOnce="{
-              opacity: 1,
-              scale: 0.9,
-              transition: {
-                type: 'spring',
-                stiffness: '100',
-                delay: 500,
-              },
-            }"
-          >
-            <div>Welcome aboard!</div>
-            <div>
-              Prepare for an adventure of a lifetime as we embark on the journey
-              of love together.
-            </div>
-          </div>
-          <div
-            class="text-2xl text-white lg:text-6xl drop-shadow font-cursive"
-            v-motion
-            :initial="{
-              opacity: 0,
-              scale: 0,
-            }"
-            :visibleOnce="{
-              opacity: 1,
-              scale: 0.9,
-              transition: {
-                type: 'spring',
-                stiffness: '100',
-                delay: 800,
-              },
-            }"
-          >
-            Get ready to stamp your passport and join us as we,
-          </div>
-          <div class="h-[50px] lg:h-[200px] p-6"></div>
-
-          <div
-            class="flex flex-col mt-2 text-[2.60rem] font-bold lg:gap-3 lg:-mt-10 text-amber-50 drop-shadow lg:text-7xl euphoria-script-regular lg:flex-row"
-            v-motion
-            :initial="{
-              opacity: 0,
-              scale: 0,
-            }"
-            :visibleOnce="{
-              opacity: 1,
-              scale: 0.9,
-              transition: {
-                type: 'spring',
-                stiffness: '100',
-                delay: 1200,
-              },
-            }"
-          >
-            Dan Joseph & Katherine
-          </div>
-
-          <div
-            class="-mt-6 text-2xl lg:text-6xl drop-shadow font-cursive lg:mt-0"
-            v-motion
-            :initial="{
-              opacity: 0,
-              scale: 0,
-            }"
-            :visibleOnce="{
-              opacity: 1,
-              scale: 0.9,
-              transition: {
-                type: 'spring',
-                stiffness: '100',
-                delay: 1500,
-              },
-            }"
-          >
-            exchange vows and celebrate this beautiful voyage on
-          </div>
-        </div>
-
-        <div
-          class="flex flex-col items-center justify-center w-full text-2xl uppercase lg:mt-6 lg:text-3xl drop-shadow font-name2"
+          class="text-2xl text-white lg:text-6xl drop-shadow font-cursive"
           v-motion
           :initial="{
             opacity: 0,
@@ -150,48 +67,132 @@ sound.on("end", function () {
             transition: {
               type: 'spring',
               stiffness: '100',
-              delay: 2000,
+              delay: 500,
             },
           }"
         >
-          <div class="lg:pl-0">June</div>
-          <div class="flex items-center justify-between w-[400px] lg:w-[450px]">
-            <div class="w-1/2 p-2 border-amber-400 border-y-2">Saturday</div>
-            <div class="p-2 mb-1 text-6xl font-bold text-amber-500">22</div>
-            <div class="w-1/2 p-2 border-amber-400 border-y-2">1:00PM</div>
+          <div>Welcome aboard!</div>
+          <div>
+            Prepare for an adventure of a lifetime as we embark on the journey
+            of love together.
           </div>
-          <div class="lg:pl-0">2024</div>
         </div>
-
         <div
-          class="flex flex-col gap-2 mt-2 lg:mt-6"
+          class="text-2xl text-white lg:text-6xl drop-shadow font-cursive"
           v-motion
           :initial="{
             opacity: 0,
-            x: -100,
+            scale: 0,
           }"
           :visibleOnce="{
             opacity: 1,
-            x: 0,
+            scale: 0.9,
             transition: {
               type: 'spring',
               stiffness: '100',
-              delay: 2300,
+              delay: 800,
             },
           }"
         >
-          <div
-            class="text-3xl lg:text-4xl font-cursive drop-shadow text-amber-500"
-          >
-            Ceremony & Reception
-          </div>
-          <div class="uppercase text-md lg:text-2xl drop-shadow font-name2">
-            Don Leopoldo Sison Convention Center, Alaminos City, Pangasinan
-          </div>
+          Get ready to stamp your passport and join us as we,
+        </div>
+        <div class="h-[50px] lg:h-[200px] p-6"></div>
+
+        <div
+          class="flex flex-col mt-2 text-[2.60rem] font-bold lg:gap-3 lg:-mt-10 text-amber-50 drop-shadow lg:text-7xl euphoria-script-regular lg:flex-row"
+          v-motion
+          :initial="{
+            opacity: 0,
+            scale: 0,
+          }"
+          :visibleOnce="{
+            opacity: 1,
+            scale: 0.9,
+            transition: {
+              type: 'spring',
+              stiffness: '100',
+              delay: 1200,
+            },
+          }"
+        >
+          Dan Joseph & Katherine
+        </div>
+
+        <div
+          class="-mt-6 text-2xl lg:text-6xl drop-shadow font-cursive lg:mt-0"
+          v-motion
+          :initial="{
+            opacity: 0,
+            scale: 0,
+          }"
+          :visibleOnce="{
+            opacity: 1,
+            scale: 0.9,
+            transition: {
+              type: 'spring',
+              stiffness: '100',
+              delay: 1500,
+            },
+          }"
+        >
+          exchange vows and celebrate this beautiful voyage on
         </div>
       </div>
-    </section>
-  </div>
+
+      <div
+        class="flex flex-col items-center justify-center w-full text-2xl uppercase lg:mt-6 lg:text-3xl drop-shadow font-name2"
+        v-motion
+        :initial="{
+          opacity: 0,
+          scale: 0,
+        }"
+        :visibleOnce="{
+          opacity: 1,
+          scale: 0.9,
+          transition: {
+            type: 'spring',
+            stiffness: '100',
+            delay: 2000,
+          },
+        }"
+      >
+        <div class="lg:pl-0">June</div>
+        <div class="flex items-center justify-between w-[400px] lg:w-[450px]">
+          <div class="w-1/2 p-2 border-amber-400 border-y-2">Saturday</div>
+          <div class="p-2 mb-1 text-6xl font-bold text-amber-500">22</div>
+          <div class="w-1/2 p-2 border-amber-400 border-y-2">1:00PM</div>
+        </div>
+        <div class="lg:pl-0">2024</div>
+      </div>
+
+      <div
+        class="flex flex-col gap-2 mt-2 lg:mt-6"
+        v-motion
+        :initial="{
+          opacity: 0,
+          x: -100,
+        }"
+        :visibleOnce="{
+          opacity: 1,
+          x: 0,
+          transition: {
+            type: 'spring',
+            stiffness: '100',
+            delay: 2300,
+          },
+        }"
+      >
+        <div
+          class="text-3xl lg:text-4xl font-cursive drop-shadow text-amber-500"
+        >
+          Ceremony & Reception
+        </div>
+        <div class="uppercase text-md lg:text-2xl drop-shadow font-name2">
+          Don Leopoldo Sison Convention Center, Alaminos City, Pangasinan
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 <style scoped>
 .euphoria-script-regular {
